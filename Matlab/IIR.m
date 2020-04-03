55fdf6d29e12935ed8b0da36ac7e2be097e89cc2
diogo0001 <diogo.t0001@gmail.com>
2020-04-03 17:58:10 -0300

    Updated matlab scripts

--- Matlab/IIR.m
+++ Matlab/IIR.m
@@ -1,75 +1,141 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-%
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear;
 clc;
 
-% Open file
-% file = 'sweep_20_1000Hz.wav';
-file = 'noise.wav';
-save_file = strcat('iir_cross_',file);
-[x,fs] = audioread(file);
-x = x(:,1);
+play = 1;
+write_file = 1;
+
+eq = 0;
+crossover = 1;
 
 % Initial values
-f0 = 200;
-G = -30;
-Q = 8;
+f0 = 201;
+G = 9 ;
+Q = 10;
 
-prev_f0 = f0;
-prev_G = G;
-prev_Q = Q;
+coef_amp = 1; %1/(2^2);
+amp = 0.8;
+% Open file
+% file = 'noise.wav';
+file = 'sweep_20_1000HZ.wav';
 
-[a b] = coef_calc(f0,G,Q,fs);
+input_folder = 'audio_files/';
+output_folder = 'outputIIR/';
+infile = strcat(input_folder,file);
 
-y(1) = b(1)*x(1);
-y(2) = b(1)*x(2) + b(2)*x(1) - a(1)*y(1);
-y(3) = b(1)*x(3) + b(2)*x(2) + b(3)*x(1) - a(1)*y(2) - a(2)*y(1);
+[audio,fs] = audioread(infile);
+audio = amp*audio(:,1);  
+% sound(audio,fs);
 
-i = 1;
-N = length(x);
-for n=4:N
+% ---------------------------------
+if eq == 1
     
-    i = i+1;
+    [a b] = eq_coef_calc(f0,G,Q,fs);
+    y = param_eq(a,b,audio,coef_amp);
     
-    if i> N/60
-        i = 1;
-        f0 = f0 + 100;
+    if write_file == 1
+        save_file = strcat(input_folder,output_folder,'iir_EQ',int2str(f0),file);
+        audiowrite(save_file, y, fs);
     end
-    
-    
-    if prev_f0 ~= f0 | prev_G ~= G | prev_Q ~= Q
-        [a b] = coef_calc(f0,G,Q,fs);
-        prev_f0 = f0;
-        prev_G = G;
-        prev_Q = Q;
+
+    if play ==1
+        sound(y,fs);
     end
-    
-     y(n) = b(1)*x(n) + b(2)*x(n-1) + b(3)*x(n-2) - a(1)*y(n-1) - a(2)*y(n-2); 
+
 end
+% --------------------------------------------
 
-y = y';
+if crossover == 1
+    
+    N = length(audio);
+    audio_f_LP = zeros(1,N);
+    audio_f_HP = zeros(1,N);
+    
+    [lp, hp] = cros_coef_calc(f0,G,Q,fs);
+    stereo_mtx = cross(lp,hp,audio,coef_amp);
 
-sound(y,fs);
+    if write_file == 1
+        save_file = strcat(input_folder,output_folder,'iir_cross_',int2str(f0),file);
+        audiowrite(save_file, stereo_mtx, fs);
+    end
+
+    if play ==1
+        sound(stereo_mtx,fs);
+    end
+    
+    if play == 2
+        sound(audio_f_HP,fs);
+        sound(audio_f_LP,fs);
+    end
+   
+end
 
 % plot(x)
 % figure(2);
 % plot(y);
 
-if prev_f0 ~= f0 | prev_G ~= G | prev_Q ~= Q
-    [a b] = coef_calc(f0,G,Q,fs);
-    prev_f0 = f0;
-    prev_G = G;
-    prev_Q = Q;
-end
 
 
 %%
-function [ca cb] = coef_calc(f0,G,Q,fs)
+function [y] = param_eq(a,b,audio,coef_amp)
+
+    a = coef_amp*a;
+    b = coef_amp*b;
+    
+    y(1) = b(1)*audio(1);
+    y(2) = b(1)*audio(2) + b(2)*audio(1) - a(1)*y(1);
+    y(3) = b(1)*audio(3) + b(2)*audio(2) + b(3)*audio(1) - a(1)*y(2) - a(2)*y(1);
+
+    N = length(audio);
+
+    for n=4:N
+         y(n) = b(1)*audio(n) + b(2)*audio(n-1) + b(3)*audio(n-2) - a(1)*y(n-1) - a(2)*y(n-2); 
+    end
+
+    y = y';
+end
+
+
+function [y] = cross(lp,hp,audio,coef_amp)
+
+    N = length(audio);
+    audio_f_LP = zeros(1,N);
+    audio_f_HP = zeros(1,N);
+        
+    lp = coef_amp*lp;
+    hp = coef_amp*hp;
+
+    % HP ----------------------------
+    b = hp(1:3)
+    a = hp(4:5)
+
+    audio_f_HP(1) = b(1)*audio(1);
+    audio_f_HP(2) = b(1)*audio(2) + b(2)*audio(1) - a(1)*audio_f_HP(1);
+    audio_f_HP(3) = b(1)*audio(3) + b(2)*audio(2) + b(3)*audio(1) - a(1)*audio_f_HP(2) - a(2)*audio_f_HP(1);
+
+    for n=4:N
+        audio_f_HP(n) = b(1)*audio(n) + b(2)*audio(n-1) + b(3)*audio(n-2) - a(1)*audio_f_HP(n-1) - a(2)*audio_f_HP(n-2); 
+    end
+    % -------------------------------
+    
+    % LP ----------------------------
+    b = lp(1:3)
+    a = lp(4:5)
+    
+    audio_f_LP(1) = b(1)*audio(1);
+    audio_f_LP(2) = b(1)*audio(2) + b(2)*audio(1) - a(1)*audio_f_LP(1);
+    audio_f_LP(3) = b(1)*audio(3) + b(2)*audio(2) + b(3)*audio(1) - a(1)*audio_f_LP(2) - a(2)*audio_f_LP(1);
+
+    for n=4:N
+        audio_f_LP(n) = b(1)*audio(n) + b(2)*audio(n-1) + b(3)*audio(n-2) - a(1)*audio_f_LP(n-1) - a(2)*audio_f_LP(n-2); 
+    end
+    % -------------------------------
+    
+    y = [audio_f_HP(:), audio_f_LP(:)];
+
+end
+
+function [ca,cb] = eq_coef_calc(f0,G,Q,fs)
 
     B = f0/Q;
     e = G/20;
@@ -86,4 +152,28 @@ function [ca cb] = coef_calc(f0,G,Q,fs)
     
     ca = [a1 a2];
     cb = [b0 b1 b2];
+end
+
+function [lp,hp] = cros_coef_calc(f0,G,Q,fs)
+
+    if Q==0
+        Q = 0.001;
+    end
+    
+    w0 = 2*pi*f0/fs;
+    alpha = sin(w0)/(2.0*Q);
+    a0 = 1 + alpha;
+    a0 = 1/a0;
+    
+    lp(1) = a0*((1-cos(w0))/2);
+    lp(2) = -2.0*lp(1);
+    lp(3) = lp(1);
+    lp(4) = -(-2.0*cos(w0))*a0;
+    lp(5) = -(1 - alpha)*a0;
+    
+    hp(1) = a0*((1+cos(w0))/2);
+    hp(2) = -2.0*hp(1);
+    hp(3) = hp(1);
+    hp(4) = -(-2.0*cos(w0))*a0;
+    hp(5) = -(1 - alpha)*a0;  
 end
