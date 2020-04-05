25702d1bb2dd00666a99d68a6bd156e0264ff932
diogo0001 <diogo.t0001@gmail.com>
2020-04-05 18:23:12 -0300

    Updated IIR.m

--- Matlab/IIR.m
+++ Matlab/IIR.m
@@ -2,22 +2,26 @@
 clear;
 clc;
 
-play = 1;
+play = 0;
 write_file = 1;
 
 eq = 0;
 crossover = 1;
 
 % Initial values
-f0 = 201;
-G = 9 ;
-Q = 10;
+f0 = 500;
+G = -9 ;
+Q = 6;
+
+f0_c = 2500;
+Q_c = 9;
 
 coef_amp = 1; %1/(2^2);
-amp = 0.8;
+amp = 0.2;
 % Open file
 % file = 'noise.wav';
-file = 'sweep_20_1000HZ.wav';
+% file = 'sweep_20_1000HZ.wav';
+file = 'sweep.wav';
 
 input_folder = 'audio_files/';
 output_folder = 'outputIIR/';
@@ -31,10 +35,12 @@ audio = amp*audio(:,1);
 if eq == 1
     
     [a b] = eq_coef_calc(f0,G,Q,fs);
-    y = param_eq(a,b,audio,coef_amp);
+%     y = param_eq(a,b,audio,coef_amp);
+    y = filter(b,a,audio);
     
     if write_file == 1
-        save_file = strcat(input_folder,output_folder,'iir_EQ',int2str(f0),file);
+        save_file = strcat(input_folder,output_folder,'iir_EQ_');
+        save_file = strcat(save_file,int2str(f0),file);
         audiowrite(save_file, y, fs);
     end
 
@@ -42,20 +48,35 @@ if eq == 1
         sound(y,fs);
     end
 
+    if crossover == 1
+        audio = y;
+    end
 end
+
 % --------------------------------------------
 
 if crossover == 1
     
     N = length(audio);
-    audio_f_LP = zeros(1,N);
-    audio_f_HP = zeros(1,N);
+    %audio_f_LP = zeros(1,N);
+    %audio_f_HP = zeros(1,N);
     
-    [lp, hp] = cros_coef_calc(f0,G,Q,fs);
-    stereo_mtx = cross(lp,hp,audio,coef_amp);
+    [lp, hp] = cros_coef_calc(f0_c,G,Q_c,fs);
+%     stereo_mtx = cross(lp,hp,audio,coef_amp);
+    
+    bh = hp(1:3)
+    ah = hp(4:6)
+    audio_f_HP = filter(bh,ah,audio);
+    
+    bl = lp(1:3) %*1.0e03
+    al = lp(4:6)
+    audio_f_LP = filter(bl,al,audio);
+    
+    stereo_mtx = [audio_f_HP(:), audio_f_LP(:)];
 
     if write_file == 1
-        save_file = strcat(input_folder,output_folder,'iir_cross_',int2str(f0),file);
+        save_file = strcat(input_folder,output_folder,'iir_cross_');
+        save_file = strcat(save_file,int2str(f0_c),file);
         audiowrite(save_file, stereo_mtx, fs);
     end
 
@@ -76,7 +97,53 @@ end
 
 
 
-%%
+%% Cálculo dos coeficientes
+
+function [ca,cb] = eq_coef_calc(f0,G,Q,fs)
+
+    B = f0/Q;
+    e = G/20;
+
+    b = -cos(2*pi*(f0/fs));
+    a = (1-tan(pi*B/fs))/((1+tan(pi*B/fs)));
+    K = 10^e;
+    
+    b0 = (1+a+K-K*a)*0.5;
+    b1 = (b+b*a);
+    b2 = (1+a-K+K*a)*0.5;
+    a1 = b1;
+    a2 = a;
+    
+    ca = [1 a1 a2];
+    cb = [b0 b1 b2];
+end
+
+function [lp,hp] = cros_coef_calc(f0,G,Q,fs)
+
+    if Q==0
+        Q = 0.001;
+    end
+    
+    w0 = 2*pi*f0/fs;
+    alpha = sin(w0)/(2.0*Q);
+    a0 = 1 + alpha;
+    
+    lp(1) = ((1-cos(w0))/2);
+    lp(2) = (1-cos(w0));
+    lp(3) = lp(1);
+    lp(4) = a0;
+    lp(5) = (-2.0*cos(w0));
+    lp(6) = (1 - alpha);
+    
+    hp(1) = ((1+cos(w0))/2);
+    hp(2) = -(1+cos(w0));
+    hp(3) = hp(1);
+    hp(4) = a0;
+    hp(5) = (-2.0*cos(w0));
+    hp(6) = (1 - alpha);  
+end 
+
+% Implementação
 function [y] = param_eq(a,b,audio,coef_amp)
 
     a = coef_amp*a;
@@ -135,45 +202,3 @@ function [y] = cross(lp,hp,audio,coef_amp)
 
 end
 
-function [ca,cb] = eq_coef_calc(f0,G,Q,fs)
-
-    B = f0/Q;
-    e = G/20;
-
-    b = -cos(2*pi*(f0/fs));
-    a = (1-tan(pi*B/fs))/((1+tan(pi*B/fs)));
-    K = 10^e;
-    
-    b0 = (1+a+K-K*a)*0.5;
-    b1 = (b+b*a);
-    b2 = (1+a-K+K*a)*0.5;
-    a1 = b1;
-    a2 = a;
-    
-    ca = [a1 a2];
-    cb = [b0 b1 b2];
-end
-
-function [lp,hp] = cros_coef_calc(f0,G,Q,fs)
-
-    if Q==0
-        Q = 0.001;
-    end
-    
-    w0 = 2*pi*f0/fs;
-    alpha = sin(w0)/(2.0*Q);
-    a0 = 1 + alpha;
-    a0 = 1/a0;
-    
-    lp(1) = a0*((1-cos(w0))/2);
-    lp(2) = -2.0*lp(1);
-    lp(3) = lp(1);
-    lp(4) = -(-2.0*cos(w0))*a0;
-    lp(5) = -(1 - alpha)*a0;
-    
-    hp(1) = a0*((1+cos(w0))/2);
-    hp(2) = -2.0*hp(1);
-    hp(3) = hp(1);
-    hp(4) = -(-2.0*cos(w0))*a0;
-    hp(5) = -(1 - alpha)*a0;  
-end
