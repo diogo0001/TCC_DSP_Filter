1dafad93cca912fa0faba15be53201b78d1209dd
diogo0001 <diogo.t0001@gmail.com>
2020-04-14 21:02:49 -0300

    Inverted HP polarization

--- Matlab/IIR.m
+++ Matlab/IIR.m
@@ -1,25 +1,33 @@
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Author: Diogo Tavares
+%   Project: Variable 2 band crossover with equalization
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear;
 clc;
+
 % Options ------------
 play = 0;
-write_file = 0;
+write_file = 2; % 1 single or 2 for cascade crossover
 
-eq = 0;
+eq = 1;
 crossover = 1;
+
 
 % Filter parameters -------------------------------------------------------
 
 % Equalizer ----------
-f0 = 500;
-G = -9 ;
+f0 = 350;
+G = 3 ;
 Q = 6;
 
 % Crossover ----------
-f0_c = 200;
-Q_c = 0.85;   %0.7109;   -3db   0.707 -> Butterrworth 2th order
+f0_c = 350;
+Q_c = 0.5;   
+%-3db   0.707 -> Butterrworth 2th order
+%-6db   0.5   -> Butterrworth 2th order
 
-types = ['butt-2nd', 'butt-4th', 'link-riley-2nd', 'link-riley-4th'];
 type = 'butt-2nd';
 
 amp = 0.6;
@@ -36,14 +44,13 @@ infile = strcat(input_folder,file);
 [audio,fs] = audioread(infile);
 audio = amp*audio(:,1);  
 % sound(audio,fs);
+fig = 1;
 
 % Param EQ ----------------------------------------------------------------
 if eq == 1
     
     [a b] = eq_coef_calc(f0,G,Q,fs);
-%     y = param_eq(a,b,audio,coef_amp);
-    
-    analise(b,a,3,1,0,0,fs)
+    analise(b,a,fig,0,0,0,fs);
     
     y = filter(b,a,audio);
     
@@ -66,7 +73,7 @@ end
 % Crossover ---------------------------------------------------------------
 
 if crossover == 1
-    
+    fig = fig + 1;
     N = length(audio);
     [lp, hp] = cros_coef_calc(f0_c,G,Q_c,fs,type);
     bl = lp(1:3) %*1.0e03
@@ -78,24 +85,26 @@ if crossover == 1
     audio_f_HP = filter(bh,ah,audio);
     
 % Transfer function analisis -----------------------------
-    [ftma_L,ftmf_L] = analise(bl,al,3,0,0,0,fs)
-    [ftma_H,ftmf_H] = analise(bh,ah,3,0,0,0,fs)
+    [ftma_L,ftmf_L] = analise(bl,al,fig,0,0,0,fs);
+    [ftma_H,ftmf_H] = analise(bh,ah,fig,0,0,0,fs);
     options = bodeoptions;
     options.FreqUnits = 'Hz';
     options.Grid = 'on';
     options.Xlim = [10 20000];
     options.MagLowerLimMode ='manual';
-%     options.Ylim = [-100 10];
     options.MagLowerLim = -100;
-  
+    
+    figure(fig)
     bode(ftma_L,options);
     hold on;
+    bode(-ftma_H,options);  % Inverted the polarization 
+    hold on;
     bode(ftma_H,options);
-
+    
     stereo_mtx = [audio_f_HP(:), audio_f_LP(:)];
     ch_sum = audio_f_HP + audio_f_LP;
 
-    if write_file == 1
+    if write_file == 1 || write_file == 2
         save_file = strcat(input_folder,output_folder,'iir_st_cross_');
         save_file = strcat(save_file,int2str(f0_c),file);
         audiowrite(save_file, stereo_mtx, fs);
@@ -156,9 +165,9 @@ function [lp,hp] = cros_coef_calc(f0,G,Q,fs,type)
             lp(5) = (-2.0*cos(w0));
             lp(6) = (1 - alpha);
 
-            hp(1) = ((1+cos(w0))/2);
-            hp(2) = -(1+cos(w0));
-            hp(3) = hp(1);
+            hp(1) = -((1+cos(w0))/2);   % Inverted the polarization
+            hp(2) = (1+cos(w0));        % Inverted the polarization
+            hp(3) = hp(1);              % Inverted the polarization
             hp(4) = a0;
             hp(5) = (-2.0*cos(w0));
             hp(6) = (1 - alpha);  
@@ -203,7 +212,6 @@ function [ftma,ftmf] = analise(num, den,fig,bod,rlocus,step,fs)
         options.Grid = 'on';
         options.MagLowerLimMode ='manual'
         options.Xlim = [20 20000]; 
-%    options.Ylim = [-100 10];
         options.MagLowerLim = -80;
         figure(fig)
         bode(ftma,options);
