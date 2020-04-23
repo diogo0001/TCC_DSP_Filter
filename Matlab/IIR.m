1fb701041d39e7d6b949f23e716a153539ad400e
diogo0001 <diogo.t0001@gmail.com>
2020-04-22 21:48:55 -0300

    Created frequency variator

--- Matlab/IIR.m
+++ Matlab/IIR.m
@@ -7,55 +7,66 @@
 clear;
 clc;
 
-% Options ------------
+% Options -----------------------------------------------------------------
 play = 0;
-write_file = 2; % 1 single or 2 for cascade crossover
+write_file = 1; % 1 single or 2 for cascade crossover
+plot = 0;
 
 eq = 1;
-crossover = 1;
+vari = 1;
 
+crossover = 0;
 
 % Filter parameters -------------------------------------------------------
 
 % Equalizer ----------
-f0 = 350;
-G = 3 ;
-Q = 6;
+f0 = [80,200];
+G = [9,3] ;
+Q = [3,9];
 
 % Crossover ----------
-f0_c = 350;
-Q_c = 0.5;   
-%-3db   0.707 -> Butterrworth 2th order
-%-6db   0.5   -> Butterrworth 2th order
+f0_c = 500;
 
-type = 'butt-2nd';
+type = 'Link'
+% type = 'Butt';
 
-amp = 0.6;
 
-% Files --------------
-% file = 'noise.wav';
-file = 'sweep_20_1000HZ.wav';
+% Files -------------------------------------------------------------------
+file = 'noise.wav';
+% file = 'sweep_20_1000HZ.wav';
 % file = 'sweep.wav';
 
 input_folder = 'audio_files/';
 output_folder = 'outputIIR/';
 infile = strcat(input_folder,file);
 
+amp = 0.5;
 [audio,fs] = audioread(infile);
 audio = amp*audio(:,1);  
 % sound(audio,fs);
+
 fig = 1;
+file = strcat(type,file);
+
 
 % Param EQ ----------------------------------------------------------------
 if eq == 1
     
-    [a b] = eq_coef_calc(f0,G,Q,fs);
-    analise(b,a,fig,0,0,0,fs);
+    y = audio;  % Preserves the audio input
     
-    y = filter(b,a,audio);
+    if vari ==1   
+        file = strcat('_vari_',file);
+        y = eq_variator(y,50,2000,fs,20);
+    else    
+        % For multiple frequencies
+        for i=1:length(f0)
+            [a b] = eq_coef_calc(f0(i),G(i),Q(i),fs);
+            y = filter(b,a,y);
+        end
+    end
     
     if write_file == 1
-        save_file = strcat(input_folder,output_folder,'iir_EQ_');
+        save_file = strcat(input_folder,output_folder,'_EQ_');
         save_file = strcat(save_file,int2str(f0),file);
         audiowrite(save_file, y, fs);
     end
@@ -65,6 +76,7 @@ if eq == 1
     end
 
     if crossover == 1
+        file = strcat(file,'_EQ_');
         audio = y;
     end
 
@@ -73,20 +85,7 @@ end
 % Crossover ---------------------------------------------------------------
 
 if crossover == 1
-    fig = fig + 1;
-    N = length(audio);
-    [lp, hp] = cros_coef_calc(f0_c,G,Q_c,fs,type);
-    bl = lp(1:3) %*1.0e03
-    al = lp(4:6)
-    bh = hp(1:3)
-    ah = hp(4:6)
     
-    audio_f_LP = filter(bl,al,audio);    
-    audio_f_HP = filter(bh,ah,audio);
-    
-% Transfer function analisis -----------------------------
-    [ftma_L,ftmf_L] = analise(bl,al,fig,0,0,0,fs);
-    [ftma_H,ftmf_H] = analise(bh,ah,fig,0,0,0,fs);
     options = bodeoptions;
     options.FreqUnits = 'Hz';
     options.Grid = 'on';
@@ -94,22 +93,89 @@ if crossover == 1
     options.MagLowerLimMode ='manual';
     options.MagLowerLim = -100;
     
-    figure(fig)
-    bode(ftma_L,options);
-    hold on;
-    bode(-ftma_H,options);  % Inverted the polarization 
-    hold on;
-    bode(ftma_H,options);
+    fig = fig + 1;
+    N = length(audio);
+    
+    % Butterwoth 2nd order
+    Q_c = 0.5;
+    [lp, hp] = cros_coef_calc(f0_c,G,Q_c,fs);
+    bl = lp(1:3) %*1.0e03
+    al = lp(4:6)
+    bh = -hp(1:3)  % Inverted polarization
+    ah = hp(4:6)
+
+    if strcmp(type,'Butt') 
+        audio_f_LP = filter(bl,al,audio);    % section 1 2nd order
+        audio_f_HP = filter(bh,ah,audio);
+    end
+
+    [ftBt_L,ftmf_L] = analise(bl,al,fig,0,0,0,fs);
+    [ftBt_H,ftmf_H] = analise(bh,ah,fig,0,0,0,fs);
+    
+    if plot ==1
+        figure(1)
+        bode(ftBt_L,options);
+        hold on;
+        bode(ftBt_H,options);  
+        bode(ftBt_L+ftBt_H,options); 
+    end
+
+    % Linkwitz-Riley 4th order
+    Q_c = 0.707;
+    [lp, hp] = cros_coef_calc(f0_c,G,Q_c,fs);
+    bl = lp(1:3) %*1.0e03
+    al = lp(4:6)
+    bh = hp(1:3)
+    ah = hp(4:6)
+
+    if strcmp(type,'Link')
+        audio_f_LP1 = filter(bl,al,audio);       % section 1 2nd order
+        audio_f_HP1 = filter(bh,ah,audio);
+        audio_f_LP = filter(bl,al,audio_f_LP1);  % section 2 2nd order   
+        audio_f_HP = filter(bh,ah,audio_f_HP1);
+    end
+
+    [ftLR_L,ftmf_L] = analise(bl,al,fig,0,0,0,fs);
+    [ftLR_H,ftmf_H] = analise(bh,ah,fig,0,0,0,fs);
+    ftLR_L = ftLR_L*ftLR_L;
+    ftLR_H = ftLR_H*ftLR_H;
+    
+    if plot ==1
+        figure(2)
+        bode(ftLR_L,options);
+        hold on;
+        bode(ftLR_H,options);  
+        bode(ftLR_L+ftLR_H,options); 
+
+        figure(3)
+        bode(ftLR_L,options);
+        hold on;
+        bode(ftLR_H,options);  
+        bode(ftLR_L+ftLR_H,options);
+
+        bode(ftBt_L,options);
+        bode(ftBt_H,options);  
+        bode(ftBt_L+ftBt_H,options); 
+    end
+    
+%     [nx,dx] = tfdata(ftLR_L);
+%     [n,d]=freqz(nx{1},dx{1});
+%     hold on;
+%     [nx,dx] = tfdata(ftLR_H);
+%     freqz(nx{1},dx{1});
+%     [nx,dx] = tfdata(ftLR_L+ftLR_H);
+%     freqz(nx{1},dx{1});
+%     plot(nx{1});
     
     stereo_mtx = [audio_f_HP(:), audio_f_LP(:)];
     ch_sum = audio_f_HP + audio_f_LP;
 
     if write_file == 1 || write_file == 2
-        save_file = strcat(input_folder,output_folder,'iir_st_cross_');
+        save_file = strcat(input_folder,output_folder,'_st_cross_');
         save_file = strcat(save_file,int2str(f0_c),file);
         audiowrite(save_file, stereo_mtx, fs);
         
-        save_file = strcat(input_folder,output_folder,'iir_sum_cross_');
+        save_file = strcat(input_folder,output_folder,'_sum_cross_');
         save_file = strcat(save_file,int2str(f0_c),file);
         audiowrite(save_file, ch_sum, fs);
     end
@@ -122,7 +188,11 @@ if crossover == 1
         sound(audio_f_HP,fs);
         sound(audio_f_LP,fs);
     end
-   
+    
+    figure(5)        
+    plot_fft(audio,fs,'db');
+    hold on;
+    plot_fft(ch_sum,fs,'db')
 end
 
 %% Cálculo dos coeficientes -----------------------------------------------
@@ -146,39 +216,29 @@ function [ca,cb] = eq_coef_calc(f0,G,Q,fs)
     cb = [b0 b1 b2];
 end
 
-function [lp,hp] = cros_coef_calc(f0,G,Q,fs,type)
+function [lp,hp] = cros_coef_calc(f0,G,Q,fs)
 
     if Q==0
         Q = 0.001;
     end
-       
-    switch type
-        case 'butt-2nd'
-            w0 = 2*pi*f0/fs;
-            alpha = sin(w0)/(2.0*Q);
-            a0 = 1 + alpha;
+        w0 = 2*pi*f0/fs;
+        alpha = sin(w0)/(2.0*Q);
+        a0 = 1 + alpha;
 
-            lp(1) = ((1-cos(w0))/2);
-            lp(2) = (1-cos(w0));
-            lp(3) = lp(1);
-            lp(4) = a0;
-            lp(5) = (-2.0*cos(w0));
-            lp(6) = (1 - alpha);
+        lp(1) = ((1-cos(w0))/2);
+        lp(2) = (1-cos(w0));
+        lp(3) = lp(1);
+        lp(4) = a0;
+        lp(5) = (-2.0*cos(w0));
+        lp(6) = (1 - alpha);
 
-            hp(1) = -((1+cos(w0))/2);   % Inverted the polarization
-            hp(2) = (1+cos(w0));        % Inverted the polarization
-            hp(3) = hp(1);              % Inverted the polarization
-            hp(4) = a0;
-            hp(5) = (-2.0*cos(w0));
-            hp(6) = (1 - alpha);  
+        hp(1) = ((1+cos(w0))/2); 
+        hp(2) = -(1+cos(w0));        
+        hp(3) = hp(1);             
+        hp(4) = a0;
+        hp(5) = (-2.0*cos(w0));
+        hp(6) = (1 - alpha);  
 
-        case 'butt-4th'
-        case 'link-riley-2nd'
-        case 'link-riley-4th'
-        otherwise
-            lp = 0;
-            hp = 0;
-    end 
 end
 
 function [ftma,ftmf] = analise(num, den,fig,bod,rlocus,step,fs)
@@ -216,4 +276,73 @@ function [ftma,ftmf] = analise(num, den,fig,bod,rlocus,step,fs)
         figure(fig)
         bode(ftma,options);
     end
+end
+
+function buffer = eq_variator(sig,f_ini,f_end,fs,inc_f)
+
+    G = 12;
+    Q = 6;
+    sig = sig';
+    size = length(sig);
+    step_f = -(f_ini - f_end);
+    step_f = floor(step_f/inc_f);
+    step_t = floor(size/abs(step_f)); 
+    buffer = [];
+    f0 = f_ini;
+        
+    for i=1:step_t:size
+        if size-i <= step_t
+            break
+        end
+        
+        f0 = f0 + inc_f;
+        [a,b] = eq_coef_calc(f0,G,Q,fs);
+        buffer = [buffer filter(b,a,sig(i:i+step_t-1))];
+    end
+    
+    buffer = [buffer filter(b,a,sig(i:end))];
+    buffer = buffer';
+end
+% Função criada para uso generico de plotagem de graficos FFT
+
+function [fft_max,sig_max] =  plot_fft(sig,fs,xgrid)
+
+	m = length(sig); 
+	n = pow2(nextpow2(m));
+	y = fft(sig,n); 
+    
+	f = (0:n-1)*(fs/n); 
+	power = abs(y).^2/n;  
+
+	mxy = max(y);
+	fft_max = abs(real(mxy));
+	sig_max = max(sig);
+	mx = fft_max;
+
+	switch (xgrid)
+
+		case 'db'
+            loglog(f(1:floor(n/2)),power(1:floor(n/2)))
+% 			axis([1 20000 10e-5 mx]); 
+			grid on
+			title('Resposta em frequencia')
+			xlabel('Frequencia (Hz)')
+			ylabel('Amplitude (db)')
+
+		case 'log'
+			semilogx(f(1:floor(n/2)),power(1:floor(n/2)))
+			%axis([1 20000 0 mx]); 
+			grid on
+			title('Resposta em frequencia')
+			xlabel('Frequencia (Hz)')
+			ylabel('Amplitude')
+		case 'lin'
+			plot(f(1:floor(n/2)),power(1:floor(n/2)))
+			%axis([1 20000 0 mx]); 
+			grid on
+			title('Resposta em frequencia')
+			xlabel('Frequencia (Hz)')
+			ylabel('Amplitude')
+		end
+
 end
